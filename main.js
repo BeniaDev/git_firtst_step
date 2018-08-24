a9815dbfb85d250df904b614b3c036c4caba3c42
Eugene Borisov <joniboriso@outlook.com>
2018-08-24 11:11:01 +0700

    calc_f.02

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const input = document.getElementById('num-input');
+var num = 0;
+let inOp2 = false;
+let inOp3 = false;
+let inOp4 = false;
+let inOp5 = false;
+const point = document.getElementById('num.');
+const plus_button = document.getElementById('num+');
+const minus_button = document.getElementById('num-');
+const backspaceButton = document.getElementById('bs');
+const div_button = document.getElementById('num/');
+const inc_button = document.getElementById('num*');
+const nums = document.getElementsByClassName('num-button');
 
 enterNumber = (n) => {
     if (input.value === '0') {
@@ -8,73 +20,6 @@ enterNumber = (n) => {
     }
 };
 
-const point = document.getElementById('num.');
-point.onclick = () => {
-    for (let i = 0; i < input.value.length; i++) {
-        if (input.value[i] === '.') {
-            return;
-        }
-    }
-    enterNumber('.');
-};
-const plus_button = document.getElementById('num+');
-var inOp = false;
-var num = 0;
-plus_button.onclick = () => {
-    if (inOp) {
-        input.value = parseFloat(input.value) + num;
-        num = 0;
-        inOp = false;
-    }
-    else {
-        num = parseFloat(input.value);
-        inOp = true;
-        input.value = 0;
-    }
-};
-
-const minus_button = document.getElementById('num-');
-
-minus_button.onclick = () => {
-    if (inOp) {
-        input.value = num - parseFloat(input.value);
-        num = 0;
-        inOp = false;
-    }
-    else {
-        num = parseFloat(input.value);
-        inOp = true;
-        input.value = 0;
-    }
-};
-const div_button = document.getElementById('num/');
-div_button.onclick = () => {
-    if (inOp) {
-        input.value =  num / parseFloat(input.value) ;
-        num = 0;
-        inOp = false;
-    }
-    else {
-        num = parseFloat(input.value);
-        inOp = true;
-        input.value = 0;
-    }
-};
-const inc_button = document.getElementById('num*');
-inc_button.onclick = () => {
-    if (inOp) {
-        input.value =  num * parseFloat(input.value) ;
-        num = 0;
-        inOp = false;
-    }
-    else {
-        num = parseFloat(input.value);
-        inOp = true;
-        input.value = 0;
-    }
-};
-
-const backspaceButton = document.getElementById('bs');
 backspaceButton.onclick = () => {
     if (input.value.length === 1) {
         input.value = '0'
@@ -84,11 +29,81 @@ backspaceButton.onclick = () => {
     }
 };
 
-const numButtons = document.getElementsByClassName('num-button');
-for (let i = 0; i < numButtons.length; i++) {
-    const button = numButtons[i];
-    const num = button.textContent;
-    button.onclick = () => {
-        enterNumber(num)
+
+point.onclick = () => {
+    if (input.value === '0') {
+        return input.value += '.';
     }
+    for (let i = 0; i < input.value.length; i++) {
+        if (input.value[i] === '.') {
+            return;
+        }
+    }
+    enterNumber('.');
+};
+
+plus_button.onclick = () => {
+    console.log('num=', num);
+    if (!inOp2) {
+        num = parseFloat(input.value);
+        inOp2 = true;
+        input.value = 0;
+    }
+};
+
+
+minus_button.onclick = () => {
+    console.log('num=', num);
+    if(!inOp3) {
+        num = parseFloat(input.value);
+        inOp3 = true;
+        input.value = 0;
+    }
+};
+
+
+div_button.onclick = () => {
+    console.log('num=', num);
+    if(!inOp4) {
+        num = parseFloat(input.value);
+        inOp4 = true;
+        input.value = 0;
+    }
+};
+
+inc_button.onclick = () => {
+    if(!inOp5) {
+        num = parseFloat(input.value);
+        inOp5 = true;
+        input.value = 0;
+    }
+};
+
+
+
+for (let i =0; i < nums.length; i++) {
+    nums[id = 'num' + i].onclick = () => {
+        enterNumber(nums[id = 'num' + i].value);
+        if (inOp2) {
+            input.value = parseFloat(input.value) + num;
+            num = 0;
+            inOp2 = false;
+        }
+        if (inOp3) {
+            input.value = num - parseFloat(input.value);
+            num = 0;
+            inOp3 = false;
+        }
+        if (inOp4) {
+            input.value = num / parseFloat(input.value);
+            num = 0;
+            inOp4 = false;
+        }
+        if (inOp5) {
+            input.value = num * parseFloat(input.value);
+            num = 0;
+            inOp5 = false;
+        }
+    };
 }
+
